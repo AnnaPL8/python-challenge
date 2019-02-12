e4b8b4e5f4b8a16f9f03dd2bc7e674a8c4db17b2
Anna Miller <annamiller8888@gmail.com>
2019-02-12 15:11:28 -0800

    second commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,3 +3,37 @@ import pandas as pd
 df = pd.read_csv("Resources/budget_data.csv")
 #head() will check for the top 5 values
 print(df.head())
+print(df.shape)
+size = df.shape
+print(size[0])
+
+total_months = size[0]
+
+second_col = df["Profit/Losses"]
+
+print(second_col)
+
+print(second_col.sum())
+
+print(df["Profit/Losses"].diff())
+
+print(df["Profit/Losses"].diff().mean())
+
+amount = df["Profit/Losses"].diff().max()
+
+
+print(amount)
+
+index = df["Profit/Losses"].diff().idxmax()
+
+month = df["Date"][index]
+
+decrease_amount = df["Profit/Losses"].diff().min()
+
+index_decrease = df["Profit/Losses"].diff().idxmin()
+
+decrease_month =  df["Date"][index_decrease]
+
+
+
+
